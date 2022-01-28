52e8ca9838eba6eb780088d4db2049a6b1dd2e33
Mandalore From Space <shemeershah789@gmail.com>
2022-01-28 11:57:54 +0530

    put route added

--- app.js
+++ app.js
@@ -80,6 +80,20 @@ app.route("/articles/:articleTitle")
   });
 })
 
+.put(function(req, res){
+
+  Article.updateOne(
+    {title: req.params.articleTitle},
+    {title: req.body.title, content: req.body.content},
+    function(err){
+      if (!err){
+        res.send("successfully updated Article.");
+      }
+    }
+  );
+
+});
+
 
 //server port listen
 app.listen(3000, function(){
