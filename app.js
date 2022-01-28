66b163b6fc5bc777b2e8e59adcc3ba0e10c9cc40
Mandalore From Space <shemeershah789@gmail.com>
2022-01-29 00:52:50 +0530

    specific file delete method completed

--- app.js
+++ app.js
@@ -106,6 +106,20 @@ app.route("/articles/:articleTitle")
       }
     }
   );
+})
+
+.delete(function(req, res){
+  Article.deleteOne(
+    {title: req.params.articleTitle},
+    function(err){
+      if(!err){
+        res.send("successfully deleted the corresponding article");
+      }
+      else {
+        res.send(err);
+      }
+    }
+  );
 });
 
 
